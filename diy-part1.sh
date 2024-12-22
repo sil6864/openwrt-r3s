47e730a8103ad0fe8438d4fb739af39aad026def
lishisheng0 <1094346379@qq.com>
2024-12-22 09:12:59 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #=================================================
 
-sed -i "s/192.168.1/10.0.0/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1/192.168.0/" package/base-files/files/bin/config_generate
 
 sed -i "/mediaurlbase/d" package/feeds/*/luci-theme*/root/etc/uci-defaults/*
