31b09c695a50ef11b187c8470cbc6df3b0d9eae3
lishisheng0 <1094346379@qq.com>
2024-12-28 16:12:12 +0800

    增加luci-app-upnp

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #=================================================
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' 
+sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
