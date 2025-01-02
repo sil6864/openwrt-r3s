bc93501305c2a894b3708e59371329c4077a2942
lishisheng0 <1094346379@qq.com>
2025-01-02 16:44:16 +0800

    取消telephony feed

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #=================================================
+sed -i '/telephony/d' feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
