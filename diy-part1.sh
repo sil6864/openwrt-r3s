038d40ba04800df676135d3fae390e7320cf3a89
lishisheng0 <1094346379@qq.com>
2025-01-03 10:05:15 +0800

    还原telephony，修改dockerman版本

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #=================================================
-sed -i '/telephony/d' feeds.conf.default
 sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
