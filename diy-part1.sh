9be9754bb834e56bcb1d8b4638c1a2fde7b9bce5
lishisheng0 <1094346379@qq.com>
2025-03-25 11:54:15 +0800

    .config 移除passwall

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #=================================================
-sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+#sed -i '$a src-git kiddin9 https://github.com/kiddin9/kwrt-packages' feeds.conf.default
+sed -i '$a src-git kiddin9 https://github.com/lishisheng0/kwrt-packages-xx' feeds.conf.default
